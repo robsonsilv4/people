b86b7ef3ec21765ea7c61a0ea59e8234aa041cac
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-26 14:24:39 -0300

    Renderizando uma lista

--- App.js
+++ App.js
@@ -4,11 +4,27 @@ import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/Components/Header";
 
 export default class App extends React.Component {
+  renderList() {
+    const names = [
+      "Robson Silva",
+      "Gildard Mhn",
+      "Gabriel Lins",
+      "Max Wendel",
+      "Emerson Vieira"
+    ];
+
+    textElements = names.map(name => {
+      return <Text>{name}</Text>;
+    });
+
+    return textElements;
+  }
+
   render() {
     return (
       <View>
         <Header title="Pessoas!" />
-        <Text>Funcionando!</Text>
+        {this.renderList()}
       </View>
     );
   }
