db91a8bc06d95ac1bdd989d3399674917f655706
robsonsilv4 <robsonsilv410@gmail.com>
2019-01-31 13:53:24 -0300

    Loading com ActivityIndicator

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
 import PeopleList from "../components/PeopleList";
 
@@ -9,24 +9,31 @@ export default class PeoplePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      peoples: []
+      peoples: [],
+      loading: false
     };
   }
 
   componentDidMount() {
-    axios
-      .get("https://randomuser.me/api/?nat=br&results=150")
-      .then(response => {
-        const { results } = response.data;
-        this.setState({
-          peoples: results
+    this.setState({ loading: true });
+
+    setTimeout(() => {
+      axios
+        .get("https://randomuser.me/api/?nat=br&results=150")
+        .then(response => {
+          const { results } = response.data;
+          this.setState({
+            peoples: results,
+            loading: false
+          });
         });
-      });
+    }, 1500);
   }
 
   render() {
     return (
       <View>
+        <ActivityIndicator size="large" color="#6ca2f7" />
         <PeopleList
           peoples={this.state.peoples}
           onPressItem={pageParams => {
