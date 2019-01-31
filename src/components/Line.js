ef7139f2c9e7833d7085882f2ce5f56aef3c0708
robsonsilv4 <robsonsilv410@gmail.com>
2019-01-31 10:18:06 -0300

    Deixando Line mais inteligente

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const Line = ({ label, content }) => {
+const Line = ({ label = "", content = "-" }) => {
   return (
     <View style={styles.line}>
-      <Text style={[styles.cell, styles.label]}>{label}</Text>
+      <Text
+        style={[
+          styles.cell,
+          styles.label,
+          label.length > 8 ? styles.longLabel : null
+        ]}
+      >
+        {label}
+      </Text>
       <Text style={[styles.cell, styles.content]}>{content}</Text>
     </View>
   );
@@ -28,6 +36,9 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 3
+  },
+  longLabel: {
+    fontSize: 12
   }
 });
 
